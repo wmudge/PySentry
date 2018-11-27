58c5f6cdd8c01cfa55d5ec367c06586040e5c7ff
Webster Mudge <wmudge@cloudera.com>
2018-11-27 14:36:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Wrapper for Sentry
+
+Uses the Thrift interfaces.
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+from os import path
+from io import open
+
+here = path.abspath(path.dirname(__file__))
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='PySentry',
+    version='1.0.0.dev1',
+    description='Python wrapper for the Sentry Thrift specs',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Webster Mudge',
+    author_email='wmudge@cloudera.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries'
+        'License :: OSI Approved :: Apache Software License'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='apache sentry hadoop security governance',
+    packages=['pysentry'],
+    install_requires=['thrift'],
+
+)
