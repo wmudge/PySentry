b5028e1817fc2a0e4c52baba4eaa931e4e5c99f0
Webster Mudge <wmudge@cloudera.com>
2018-11-28 14:22:37 -0500

    Update to MultiplexedProtocol

--- pysentry/client.py
+++ pysentry/client.py
@@ -2,6 +2,7 @@ from sentry_policy_service import SentryPolicyService
 from sentry_policy_service.ttypes import TCreateSentryRoleRequest, TListSentryRolesRequest
 
 from thrift.protocol.TBinaryProtocol import TBinaryProtocol
+from thrift.protocol.TMultiplexedProtocol import TMultiplexedProtocol
 from thrift.transport.TSocket import TSocket
 from thrift.transport.TTransport import TTransportException
 
@@ -19,6 +20,7 @@ class SentryClient(object):
 
     """
 
+    SERVICE_NAME = 'SentryPolicyService'
     MECHANISM = 'GSSAPI'
 
     ERR_CONNECTION = 1
@@ -32,30 +34,34 @@ class SentryClient(object):
         self.host = host
         self.port = port
         self.service = service
+        self.client = None
 
     def open(self):
         """
         Connect
         :return:
         """
-        try:
-            self.socket = TSocket(self.host, self.port)
+        if self.client is not None:
+            try:
+                self.socket = TSocket(self.host, self.port)
 
-            def sasl_factory():
-                sasl_client = sasl.Client()
-                sasl_client.setAttr('host', self.host)
-                sasl_client.setAttr('service', self.service)
-                sasl_client.init()
-                return sasl_client
+                def sasl_factory():
+                    sasl_client = sasl.Client()
+                    sasl_client.setAttr('host', self.host)
+                    sasl_client.setAttr('service', self.service)
+                    sasl_client.init()
+                    return sasl_client
 
-            self.transport = TSaslClientTransport(sasl_factory, self.MECHANISM, self.socket)
-            self.transport.open()
+                self.transport = TSaslClientTransport(sasl_factory, self.MECHANISM, self.socket)
+                self.transport.open()
 
-            self.protocol = TBinaryProtocol(self.transport)
+                # self.protocol = TBinaryProtocol(self.transport)
+                # TODO Test with MultiplexedProtocol
+                self.protocol = TMultiplexedProtocol(TBinaryProtocol(self.transport), self.SERVICE_NAME)
 
-            self.client = SentryPolicyService.Client(self.protocol)
-        except TTransportException as e:
-            raise SentryError(self.ERR_CONNECTION, str(e))
+                self.client = SentryPolicyService.Client(self.protocol)
+            except TTransportException as e:
+                raise SentryError(self.ERR_CONNECTION, str(e))
 
     def close(self):
         """
@@ -96,10 +102,10 @@ class SentryClient(object):
 
 
 class SentryError(Exception):
-    def __init__(self, code, message):
+    def __init__(self, code, message, stack=None):
         self.code = code
         self.message = message
-        # TODO Add optional stack info
+        self.stack = stack
 
     def __str__(self):
         return "(Code: {}) {}".format(self.code, self.message)
